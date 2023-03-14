07fa584483216f4c0a3b06240c6875bfe394c960
karen gevorgyan <karen-gev.22@mail.ru>
2023-03-14 08:44:31 +0300

    First Commit

---
+++ src/main/java/pro/sky/testmok/TestMokApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.testmok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestMokApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/testmok/TestMokApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.testmok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
